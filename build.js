6ab06b51d7dfdc9a27ef7aed7515e8dd47f6e5c3
Metrime <Michi@Michaels-Mac-mini.local>
2014-02-01 21:40:12 +0100

    adding build

---
+++ build.js
@@ -0,0 +1,79 @@
+var buildDestination = 'build';
+
+
+
+//css
+
+gulp.task('build-css',function(){
+	var stream = gulp.src(['libs/css/*.styl','src/assets/**/*.styl','src/**/css/*.styl'])
+	.pipe(g.footer('\n\n'))
+	.pipe(g.concat('style.css'))
+	.pipe(g.stylus())
+	.pipe(g.autoprefixer())
+	.pipe(gulp.dest(buildDestination+'/assets'));
+	return stream;
+});
+
+
+
+//js
+
+gulp.task('build-js',function(){
+	var stream = gulp.src(['libs/js/*.js','src/**/js/*.js'])
+	.pipe(g.footer(';'))
+	.pipe(g.concat('scripts.js'))
+	.pipe(gulp.dest(buildDestination+'/assets'));
+	return stream;
+});
+
+
+//html
+
+gulp.task('combine-html',function(){
+	var stream = gulp.src(['src/config/header.html','src/**/views/*.html','src/**/templates/*.html','src/config/footer*.html'])
+	.pipe(g.concat('index.html'))
+	.pipe(g.specialHtml())
+	.pipe(gulp.dest('temp'));
+	return stream;
+});
+
+gulp.task('build-html',['build-css','build-js','combine-html'],function(){
+	gulp.src(['build/assets/*.css','build/assets/*.js'],{read:false})
+	.pipe(g.inject('temp/index.html'))
+	.pipe(g.minifyHtml())
+	.pipe(gulp.dest(buildDestination));
+
+	gulp.src('temp')
+	.pipe(g.clean());
+});
+
+
+
+// assets
+
+gulp.task('build-assets',function(){
+	gulp.src(['src/assets/img/*'])
+	.pipe(gulp.dest(buildDestination+'/assets/img'));
+
+	gulp.src('src/assets/icons/*.svg')
+	.pipe(g.iconfont({fontName:'icons',appendCodepoints:true}))
+	.pipe(gulp.dest(buildDestination+'/assets/fonts/'));
+
+	gulp.src('src/assets/fonts/*')
+	.pipe(gulp.dest(buildDestination+'/assets/fonts/'));
+});
+
+
+gulp.task('clean-build',function(){
+	var stream = gulp.src('build')
+	.pipe(g.clean());
+	return stream;
+});
+
+
+
+gulp.task('build',['clean-build'],function(){
+	gulp.start('build-html','build-assets');
+});
+
+
